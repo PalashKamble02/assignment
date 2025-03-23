7d3b4a2d13c28437ebd6491eac7c9cc800127d70
palashkamble <kamblepalash356@gmail.com>
2025-03-23 13:08:20 +0530

    backend url added

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
+// const api = axios.create({
+//   baseURL: 'http://localhost:5000', // Backend URL
+// });
 const api = axios.create({
-  baseURL: 'http://localhost:5000', // Backend URL
+  baseURL: 'https://assessment-back-2m9e.onrender.com', // Backend URL
 });
+
 
 export default api;
